516d357fd9ab2503b427da8245eba7877d3c6d2b
Aaron Newton <git@clientcide.com>
2014-07-04 00:05:20 -0700

    Adding less packing and simple, static demos.

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
           }
         }
     },
-    'packager': {
+    packager: {
 
       options: {
         name: {
@@ -46,14 +46,41 @@ module.exports = function(grunt) {
 
     },
 
-    'clean': {
+    clean: {
       all: {
         src: 'mootools-*.js'
+      }
+    },
+
+    less: {
+      compileBootstrap: {
+        options: {
+          strictMath: true,
+          sourceMap: true,
+          outputSourceFiles: true,
+          sourceMapURL: 'behavior-ui-bootstrap.css.map',
+          sourceMapFilename: 'dist/css/behavior-ui-bootstrap.css.map'
+        },
+        files: {
+          'dist/css/behavior-ui-bootstrap.css': 'less/behavior-ui-bootstrap.less'
+        }
+      },
+      compileFlatUI: {
+        options: {
+          strictMath: false,
+          sourceMap: true,
+          outputSourceFiles: true,
+          sourceMapURL: 'behavior-ui-flatui.css.map',
+          sourceMapFilename: 'dist/css/behavior-ui-flatui.css.map'
+        },
+        files: {
+          'dist/css/behavior-ui-flatui.css': 'less/behavior-ui-flatui.less'
+        }
       }
     }
 
   });
 
   grunt.registerTask('default', ['bower:install', 'clean', 'packager:all']);
-
+  grunt.registerTask('less-compile', ['less:compileBootstrap', 'less:compileFlatUI']);
 };
