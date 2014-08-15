38c3959e9afacfc9f559da4f51a2a6dbad133b0b
Jon Bomgardner <jbomgard@gmail.com>
2014-08-14 21:50:10 -0700

    update grunt file to put everything in dist/dev and dist/prod. Prod folder are minimized versions.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,8 +8,14 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-bower-task');
   grunt.loadNpmTasks('grunt-debug-task');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   grunt.initConfig({
+    //properties to use
+    dev: 'dist/dev',
+    prod: 'dist/prod',
     bower: {
         install: {
           options: {
@@ -41,7 +47,7 @@ module.exports = function(grunt) {
           'Behavior-UI/*',
           'Behavior/*'
         ],
-        dest: 'dist/js/behavior-ui.js'
+        dest: '<%= dev %>/js/behavior-ui.js'
       }
 
     },
@@ -59,10 +65,10 @@ module.exports = function(grunt) {
           sourceMap: true,
           outputSourceFiles: true,
           sourceMapURL: 'behavior-ui-bootstrap.css.map',
-          sourceMapFilename: 'dist/css/behavior-ui-bootstrap.css.map'
+          sourceMapFilename: '<%= dev %>/bootstrap/css/behavior-ui-bootstrap.css.map'
         },
         files: {
-          'dist/css/behavior-ui-bootstrap.css': 'less/behavior-ui-bootstrap.less'
+          '<%= dev %>/bootstrap/css/behavior-ui-bootstrap.css': 'less/behavior-ui-bootstrap.less'
         }
       },
       compileFlatUI: {
@@ -71,10 +77,113 @@ module.exports = function(grunt) {
           sourceMap: true,
           outputSourceFiles: true,
           sourceMapURL: 'behavior-ui-flatui.css.map',
-          sourceMapFilename: 'dist/css/behavior-ui-flatui.css.map'
+          sourceMapFilename: '<%= dev %>/flat-ui/css/behavior-ui-flatui.css.map'
         },
         files: {
-          'dist/css/behavior-ui-flatui.css': 'less/behavior-ui-flatui.less'
+          '<%= dev %>/flat-ui/css/behavior-ui-flatui.css': 'less/behavior-ui-flatui.less'
+        }
+      }
+    },
+
+    copy: {
+      copyBootstrap: {
+        files: [
+          {
+            expand: true,
+            cwd: 'bower_components/bootstrap/fonts',
+            src: '**/*',
+            dest: '<%= dev %>/bootstrap/fonts/'
+          },{
+            expand: true,
+            cwd: 'assets/fonts',
+            src: '**/*',
+            dest: '<%= dev %>/bootstrap/fonts'
+          },{
+            expand: true,
+            cwd: 'assets/images',
+            src: '**/*',
+            dest: '<%= dev %>/bootstrap/images'
+          }
+        ]
+      },
+      copyFlatUI:{
+        files: [
+          {
+             expand: true,
+             cwd: 'bower_components/flat-ui-official/images/',
+             src: '**/*',
+             dest: '<%= dev %>/flat-ui/images/'
+          },{
+            expand: true,
+            cwd: 'bower_components/flat-ui-official/fonts/',
+            src: '**/*',
+            dest: '<%= dev %>/flat-ui/fonts/'
+          },{
+            expand: true,
+            cwd: 'assets/fonts',
+            src: '**/*',
+            dest: '<%= dev %>/flat-ui/fonts'
+          },{
+            expand: true,
+            cwd: 'assets/images',
+            src: '**/*',
+            dest: '<%= dev %>/flat-ui/images'
+          }
+        ]
+      },
+      copyProdBootstrap: {
+        files: [
+          {
+            expand: true,
+            cwd: '<%= dev %>/bootstrap/fonts/',
+            src: '**/*',
+            dest:'<%= prod %>/bootstrap/fonts'
+          },{
+            expand: true,
+            cwd: '<%= dev %>/bootstrap/images/',
+            src: '**/*',
+            dest:'<%= prod %>/bootstrap/images'
+          }
+        ]
+      },
+      copyProdFlatUI: {
+        files: [
+          {
+            expand: true,
+            cwd: '<%= dev %>/flat-ui/fonts/',
+            src: '**/*',
+            dest:'<%= prod %>/flat-ui/fonts'
+          },{
+            expand: true,
+            cwd: '<%= dev %>/flat-ui/images/',
+            src: '**/*',
+            dest:'<%= prod %>/flat-ui/images'
+          }
+        ]
+      }
+    },
+
+    uglify: {
+      options: {
+        compress: true,
+        preserveComments: "some"
+      },
+      prodJs: {
+        files: {
+          '<%= prod %>/js/behavior-ui.js': ['<%= dev %>/js/behavior-ui.js']
+        }
+      }
+    },
+
+    cssmin: {
+      prodBootstrap: {
+        files: {
+          '<%= prod %>/bootstrap/css/behavior-ui-bootstrap.css': ['<%= dev %>/bootstrap/css/behavior-ui-bootstrap.css']
+        }
+      },
+      prodFlatUI: {
+        files: {
+          '<%= prod %>/flat-ui/css/behavior-ui-flatui.css': ['<%= dev %>/flat-ui/css/behavior-ui-flatui.css']
         }
       }
     }
@@ -83,4 +192,21 @@ module.exports = function(grunt) {
 
   grunt.registerTask('default', ['bower:install', 'clean', 'packager:all']);
   grunt.registerTask('less-compile', ['less:compileBootstrap', 'less:compileFlatUI']);
+  grunt.registerTask('copy-assets', ['copy:copyBootstrap','copy:copyFlatUI']);
+
+  grunt.registerTask('dev', function(){
+    grunt.task.run(['default','less-compile','copy-assets']);
+  });
+
+  grunt.registerTask('prod',function(){
+    grunt.task.run(['dev',
+                    'uglify:prodJs',
+                    'cssmin:prodBootstrap',
+                    'cssmin:prodFlatUI',
+                    'copy:copyProdBootstrap',
+                    'copy:copyProdFlatUI'
+                   ]);
+  });
 };
+
+
