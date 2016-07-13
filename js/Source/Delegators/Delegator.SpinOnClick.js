111fecdfc3047a72bda10eb488ff69a73817025e
Davy Wentworth <davy.wentworth@gmail.com>
2016-07-13 13:55:01 -0700

    Add timeout option to spinOnClick Delegator.

--- js/Source/Delegators/Delegator.SpinOnClick.js
+++ js/Source/Delegators/Delegator.SpinOnClick.js
@@ -10,10 +10,15 @@ requires: [Behavior/Delegator, More/Spinner]
 Delegator.register('click', {
 
   spinOnClick: {
+    defaults: {
+      timeout: 0
+    },
     handler: function(event, element, api){
       var target = element;
       if (api.get('target')) target = api.getElement('target');
       target.spin();
+      var timeout = api.getAs(Number, 'timeout');
+      if (timeout && timeout > 0) target.unspin.delay(timeout, target);
       return target.get('spinner');
     }
   },
