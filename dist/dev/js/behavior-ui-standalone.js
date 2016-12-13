8550a77ff5c626d7d4797856c544d18dc2d4aada
Davy Wentworth <davy.wentworth@gmail.com>
2016-12-13 15:14:35 -0800

    Adding image resizing Delegator.

--- dist/dev/js/behavior-ui-standalone.js
+++ dist/dev/js/behavior-ui-standalone.js
@@ -12909,6 +12909,155 @@ HtmlTable.defineParsers({
 /*
 ---
 
+name: ImageResizer.js
+
+description: Resizes an image using a canvas, if supported.
+
+requires:
+ - Core/Options
+ - Core/Events
+
+provides: [ImageResizer]
+
+...
+*/
+
+ImageResizer = new Class({
+
+  Implements: [Options, Events],
+
+  options: {
+    // onComplete: function(dataURL){ invoked after resize complete },
+    preferredWidth: 1000
+  },
+
+  initialize: function(fileInput, options){
+    // store reference to file input
+    this.file = document.id(fileInput).files[0];
+    this.setOptions(options);
+    // create canvas and drop into DOM, hidden
+    this.canvas = new Element('canvas', {styles: {display: 'none'}}).inject(document.body);
+    // determine if canvas and toDataURL are supported in the browser
+    this.isSupported() ? this.resize() : this.handleUnsupported();
+  },
+
+  isSupported: function(){
+    return !!(this.canvas.getContext && this.canvas.getContext('2d') && this.canvas.toDataURL)
+  },
+
+  handleUnsupported: function(){
+    this.destroy();
+    this.fireEvent('error');
+  },
+
+  resize: function(){
+    // create file reader instance, watch for load, and read the file
+    this.reader = new FileReader();
+    this.reader.onload = this.read.bind(this);
+    this.reader.readAsDataURL(this.file);
+  },
+
+  read: function(event){
+    // create an image element with the src set to our read file
+    // then resize
+    this.img = new Image();
+    this.img.src = event.target.result;
+    this.img.addEvent('load', this.resizeInCanvas.bind(this));
+  },
+
+  resizeInCanvas: function(){
+    // compute ideal ratio and size, assign to canvas
+    var preferredWidth = this.options.preferredWidth;
+    // only resize if the image is wider than preferredWidth
+    var ratio = this.img.width > preferredWidth ? preferredWidth / this.img.width : 1;
+    this.canvas.width = this.img.width * ratio;
+    this.canvas.height = this.img.height * ratio;
+    var ctx = this.canvas.getContext("2d");
+    ctx.drawImage(this.img, 0, 0, this.canvas.width, this.canvas.height);
+    // read new data url for image output and call our complete event
+    this.fireEvent('complete', this.canvas.toDataURL('image/png'));
+    // clean up what we created
+    this.destroy();
+  },
+
+  destroy: function(){
+    if (this.img) this.img.destroy();
+    this.canvas.destroy();
+  }
+
+});
+
+
+/*
+---
+
+name: Delegator.ResizeImage
+
+description: Delegator for resizing images before they are uploaded
+
+requires:
+ - Behavior/Delegator
+ - ImageResizer
+
+provides: [Delegator.ResizeImage]
+
+...
+*/
+
+
+/*
+  <input data-trigger="resizeImage"
+    data-resizeimage-options="
+      preferredWidth: 500,
+      targetInput: '~input'
+    "
+  />
+  <input type="hidden" name="imgDataUrl"/>
+*/
+
+Delegator.register('change', {
+
+  resizeImage: {
+    defaults: {
+      preferredWidth: 1000,
+      targetForm: '!form'
+    },
+
+    requireAs: {
+      preferredWidth: Number,
+      targetInput: String,
+      submitOnComplete: Boolean,
+      targetForm: String
+    },
+
+    handler: function(event, element, api){
+      var submitter = function(){
+        if (api.getAs(Boolean, 'submitOnComplete')){
+          api.getElement('targetForm').fireEvent('submit').submit();
+        }
+      }
+
+      // create a new instance of our resizer
+      new ImageResizer(element, {
+        preferredWidth: api.getAs({preferredWidth: Number}),
+        onComplete: function(dataURL){
+          // and set our target input's value
+          api.getElement('targetInput').src = dataURL;
+          // unset the file input (so the big image isn't submitted)
+          element.removeProperty('value');
+          submitter();
+        },
+        onError: function(){
+          submitter();
+        }
+      });
+    }
+  }
+});
+
+/*
+---
+
 name: Behavior.InfiniteScroll
 
 description: Simplification of BS.Affix that fires AJAX delegator when scrolling to Affix element.
---
+++ js/Source/ImageResizer/Delegator.ResizeImage.js
@@ -0,0 +1,67 @@
+
+/*
+---
+
+name: Delegator.ResizeImage
+
+description: Delegator for resizing images before they are uploaded
+
+requires:
+ - Behavior/Delegator
+ - ImageResizer
+
+provides: [Delegator.ResizeImage]
+
+...
+*/
+
+
+/*
+  <input data-trigger="resizeImage"
+    data-resizeimage-options="
+      preferredWidth: 500,
+      targetInput: '~input'
+    "
+  />
+  <input type="hidden" name="imgDataUrl"/>
+*/
+
+Delegator.register('change', {
+
+  resizeImage: {
+    defaults: {
+      preferredWidth: 1000,
+      targetForm: '!form'
+    },
+
+    requireAs: {
+      preferredWidth: Number,
+      targetInput: String,
+      submitOnComplete: Boolean,
+      targetForm: String
+    },
+
+    handler: function(event, element, api){
+      var submitter = function(){
+        if (api.getAs(Boolean, 'submitOnComplete')){
+          api.getElement('targetForm').fireEvent('submit').submit();
+        }
+      }
+
+      // create a new instance of our resizer
+      new ImageResizer(element, {
+        preferredWidth: api.getAs({preferredWidth: Number}),
+        onComplete: function(dataURL){
+          // and set our target input's value
+          api.getElement('targetInput').src = dataURL;
+          // unset the file input (so the big image isn't submitted)
+          element.removeProperty('value');
+          submitter();
+        },
+        onError: function(){
+          submitter();
+        }
+      });
+    }
+  }
+});
